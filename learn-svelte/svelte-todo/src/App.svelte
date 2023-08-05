b7b42bde1fd1ff065334dc6303062374c6d4258a
Leenus Darnal <leenus.darnal@gmail.com>
2023-08-05 20:43:24 +0530

    svelte todo

---
+++ learn-svelte/svelte-todo/src/App.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  import Todos from './components/Todos.svelte'
+</script>
+
+<h1 class="text-xl">Hello World</h1>
+<Todos />
---
+++ learn-svelte/svelte-todo/src/components/Todos.svelte
@@ -0,0 +1,40 @@
+<main>
+  <h1>todos</h1>
+  <section>
+    <form>
+      <input type="checkbox" id="toggle-all" class="" />
+      <label aria-label="Mark all as complete" for="toggle-all"
+        >Mark as complete</label
+      >
+      <input
+        id="new-todo"
+        class="new-todo"
+        placeholder="what needs to be done?"
+        type="text"
+      />
+    </form>
+    <ul class="">
+      <li class="">
+        <div>
+          <div>
+            <input id="todo" class="" type="checkbox" />
+            <label aria-label="check todo" class="" for="todo" />
+          </div>
+          <span class="">Todo 1</span>
+          <button aria-label="Remove todo" class="" />
+        </div>
+        <!-- <input class="" type="text" /> -->
+      </li>
+    </ul>
+
+    <div>
+      <span>0 left</span>
+      <div>
+        <button>All</button>
+        <button>Active</button>
+        <button>Completed</button>
+      </div>
+      <button>Clear completed</button>
+    </div>
+  </section>
+</main>
