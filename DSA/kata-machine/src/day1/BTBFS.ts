445721ab7cbff9224f0354ebba7803b883127c71
Leenus Darnal <leenus.darnal@gmail.com>
2023-08-12 23:04:50 +0530

    BT-BFS && BT-Compare

--- DSA/kata-machine/src/day1/BTBFS.ts
+++ DSA/kata-machine/src/day1/BTBFS.ts
@@ -1,3 +1,15 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
-
+    const queue: (BinaryNode<number> | null)[] = [head];
+    while (queue.length) {
+        const curr = queue.shift() as BinaryNode<number>;
+        if (!curr) {
+            continue;
+        }
+        if (curr.value === needle) {
+            return true;
+        }
+        queue.push(curr?.left);
+        queue.push(curr.right);
+    }
+    return false;
 }
--- DSA/kata-machine/src/day1/CompareBinaryTrees.ts
+++ DSA/kata-machine/src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,23 @@
-export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+export default function compare(
+    a: BinaryNode<number> | null,
+    b: BinaryNode<number> | null,
+): boolean {
+    //structural check
+    if (a === null && b === null) {
+        return true;
+    }
+    // structural Check
+    if (a === null || b === null) {
+        return false;
+    }
+    // value check
+    // if (a.value !== b.value) {
+    //     return false;
+    // }
 
+    // structural and value check combined
+    if (a?.value !== b?.value) {
+        return false;
+    }
+    return compare(a.left, b.left) && compare(a.right, b.right);
 }
