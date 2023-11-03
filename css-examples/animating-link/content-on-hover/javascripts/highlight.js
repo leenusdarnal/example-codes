bd1e277016d7e0ec0f83111230fdc76de1e680bb
leenusdarnal <leenus.darnal@gmail.com>
2023-11-03 16:16:35 +0530

    content and cta on hover

---
+++ css-examples/animating-link/content-on-hover/javascripts/highlight.js
@@ -0,0 +1,16 @@
+$(function () {
+  $('.option').hover(
+    function () {
+      $('.option').removeClass('highlighted')
+      $(this).addClass('highlighted')
+    },
+    function () {
+      $(this).removeClass('highlighted')
+      setTimeout(function () {
+        if (!$('.option.highlighted').lenght) {
+          $('.option.featured').addClass('highlighted')
+        }
+      }, 500)
+    }
+  )
+})
